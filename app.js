60db9ea17a582dcf53d254e7f0202175d42074de
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:30:09 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 "use strict";
-/*Task # 2 : Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
-const personName = "Eric";
-console.log(`Hello ${personName}, would like to learn some python today?`);
+
+const { clearScreenDown } = require("readline");
+
+/* Task # 3 : Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.*/
+const personName = "fAtima sHeikh";
+console.log(`Name in Uppercase : ${personName.toUpperCase()}`);
+console.log(`Name in Lowercase : ${personName.toLowerCase()}`);
+// for title case :
+let step1 = personName.split(' ');
+let step2 = step1.map(word => word.charAt(0).toUpperCase() + word.substr(1).toLocaleLowerCase());
+let step3 = step2.join(' ');
+console.log(`Name in title case : ${step3}`);
---
+++ app.ts
@@ -0,0 +1,17 @@
+/* Task # 3 : Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.*/
+
+const personName = "fAtima sHeikh";
+
+
+console.log(`Name in Uppercase : ${personName.toUpperCase()}`);
+console.log(`Name in Lowercase : ${personName.toLowerCase()}`);
+
+// for title case :
+
+let step1 = personName.split(' ');
+
+let step2 = step1.map(word => word.charAt(0).toUpperCase() + word.substr(1).toLocaleLowerCase());
+
+let step3 = step2.join(' ');
+
+console.log(`Name in title case :${step3}`);
