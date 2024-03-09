9c1ec4410eb2db16341e3e382a35a32f34dd400b
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:50:57 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-// // Task # 41 : Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+// // Task # 42 : Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
 function show_magicians(magicians) {
     for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
-        var magician_1 = magicians_1[_i];
-        console.log(magician_1.charAt(0).toUpperCase() + magician_1.slice(1));
+        var magician = magicians_1[_i];
+        console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
     }
 }
-var magician = ["ali", "yousuf", "taimoor"];
-show_magicians(magician);
+function make_great(magicians) {
+    for (var i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + ' the great';
+    }
+}
+var magicians2 = ["ali", "yousuf", "taimoor"];
+make_great(magicians2);
+show_magicians(magicians2);
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-// // Task # 41 : Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+// // Task # 42 : Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
 
 function show_magicians(magicians: string[]): void {
 
@@ -7,5 +7,13 @@ function show_magicians(magicians: string[]): void {
     }
 }
 
-const magician: string[] = ["ali", "yousuf", "taimoor"];
-show_magicians(magician)
+function make_great(magicians: string[]): void {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + ' the great'
+
+    }
+}
+
+const magicians2: string[] = ["ali", "yousuf", "taimoor"];
+make_great(magicians2)
+show_magicians(magicians2)
