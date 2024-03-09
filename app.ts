cedf2fe29a542374774d9308ea87c43d6f583752
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:36:11 -0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
-/* Task # 12 : Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.*/
+/* Task # 13 : Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”*/
 
-let names: string[] = ["Fatima", "Kashifa", "Saba", "Amber"];
 
-console.log(`Make Sure To Subsribe To Our Channel, ${names[0]}`);
-console.log(`Make Sure To Subsribe To Our Channel, ${names[1]}`);
-console.log(`Make Sure To Subsribe To Our Channel, ${names[2]}`);
-console.log(`Make Sure To Subsribe To Our Channel, ${names[3]}`);
+let transportation: string[] = ["heavybike", "car", "bicycla", "aeroplane"];
+
+console.log(`"I would like to own a" : ${transportation[0]}`);
+console.log(`"I would like to own a" : ${transportation[1]}`);
+console.log(`"I would like to own a" : ${transportation[2]}`);
+console.log(`"I would like to own a" : ${transportation[3]}`);
