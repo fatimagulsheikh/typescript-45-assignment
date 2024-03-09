1a7aecfcbbb0436d8f37caf2c5cfaa7096aabd4c
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:35:39 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 "use strict";
-/* Task # 10 (part 2) : Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+/* Task # 12 : Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.*/
 
-Task # 11 : Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.*/
-
-//FATIMA SHEIKH
-// Monday 19-feb-2024
-
-
-//storing friends names in aan Array
-let names = ["Fatima", "Saba", "Kashifa", "Amber"];
-
-//printing names one by one
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[3]);
+let names = ["Fatima", "Kashifa", "Saba", "Amber"];
+console.log(`Make Sure To Subsribe To Our Channel, ${names[0]}`);
+console.log(`Make Sure To Subsribe To Our Channel, ${names[1]}`);
+console.log(`Make Sure To Subsribe To Our Channel, ${names[2]}`);
+console.log(`Make Sure To Subsribe To Our Channel, ${names[3]}`);
