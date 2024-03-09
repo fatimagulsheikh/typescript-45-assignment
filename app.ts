6bb8c696b72a7d111abc25d3394378c2e8e26aa6
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:44:32 -0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,28 +1,14 @@
-// // Task # 29 : Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
-// • Make a array of your three favorite fruits and call it favorite_fruits.
+// // Task # 30 : Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 
-// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 
-let favorite_fruits : string [] = ["Orange", "Mango", "Grapes", "kivi", "peach"]
+let users : string [] = ["admin", "eric", "haseeb", "nadeem", "fatima"]
 
-if (favorite_fruits.includes("Mango")) {
-    console.log ("Mango")
+for(let user of users) {
+    if (user === "admin") {
+        console.log("Hello admin, would you like to see a status report?")
+    } else {
+        console.log(` Hello ${user}, thank you for logging in again`)
+    }
 }
-
-if (favorite_fruits.includes("Apple")) {
-    console.log ("apple")
-}
-
-if (favorite_fruits.includes("Orange")) {
-    console.log ("Orange")
-}
-
-if (favorite_fruits.includes("peach")) {
-    console.log ("you are really like bananas")
-}
-
-if (favorite_fruits.includes("kivi")) {
-    console.log ("you are really like bananas")
-}
-
-
