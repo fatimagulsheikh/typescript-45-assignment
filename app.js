16bea9b926c1ab64ad07411a1a22599b1cb7efdc
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:42:10 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,48 +1,9 @@
-// // Task # 24 : More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
-// • Tests for equality and inequality with strings
-// • Tests using the lower case function
-// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
-// • Tests using "and" and "or" operators
-// • Test whether an item is in a array
-// • Test whether an item is not in a array
-var name_1 = "fatima";
-var name_2 = "fatima gul";
-var name_3 = " miss fatima gul sheikh";
-// if (name_1 == name_3){
-//    console.log("names are equal")
-// } else {
-//     console.log("names are not equal")
-// }
-// if (name_1 != name_2){
-//} console.log("names are equal")
-// }
-// if (name_1 != name_3) {
-// console.log("names are equal")
-// }
-var age_1 = 18;
-var age_2 = 22;
-// if (age_1 == 18){
-//     console.log("eligible for vote")
-// }
-// if (age_1 != 22){
-//     console.log("eligible for vote in older category")
-// }
-// if (age_1 <= age_2) {
-//     console.log("younger")
-// }
-// if (age_2 <= age_1) {
-//     console.log("older")
-// }
-// if (age_1 == 18 && age_2 == 22) {
-//     console.log(" person is eligible for vote")
-// }
-// if (age_1 == 18 || age_2 != 22) {
-//     console.log(" person is eligible not for vote")
-// }
-var country = ["pakistan", "turkey", "japan", "china"];
-// if (country.includes("pakistan")){
-//    console.log("pakistan is in country list")
-//}
-if (!country.includes("america")) {
-    console.log("america is not in an array");
-}
+// // Task # 25 : Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+// let alien_color = "green"
+// if (alien_color == "green")
+// console.log("you earn 5 points")
+var alien_color = "red";
+if (alien_color == "red")
+    console.log("no output");
--- app.ts
+++ app.ts
@@ -1,66 +1,13 @@
-// // Task # 24 : More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
-// • Tests for equality and inequality with strings
+// // Task # 25 : Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 
-// • Tests using the lower case function
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
 
-// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+// let alien_color = "green"
 
-// • Tests using "and" and "or" operators
+// if (alien_color == "green")
+// console.log("you earn 5 points")
 
-// • Test whether an item is in a array
-
-// • Test whether an item is not in a array
-
-let name_1 : string = "fatima"
-let name_2 : string = "fatima gul"
-let name_3 : string = " miss fatima gul sheikh"
-
-// if (name_1 == name_3){
-//    console.log("names are equal")
-// } else {
-//     console.log("names are not equal")
-// }
-
-// if (name_1 != name_2){
-//} console.log("names are equal")
-// }
-
-// if (name_1 != name_3) {
-// console.log("names are equal")
-// }
-
-let age_1 : number = 18
-let age_2 : number = 22
-
-// if (age_1 == 18){
-//     console.log("eligible for vote")
-// }
-
-// if (age_1 != 22){
-//     console.log("eligible for vote in older category")
-// }
-
-// if (age_1 <= age_2) {
-//     console.log("younger")
-// }
-
-// if (age_2 <= age_1) {
-//     console.log("older")
-// }
-
-// if (age_1 == 18 && age_2 == 22) {
-//     console.log(" person is eligible for vote")
-// }
-
-// if (age_1 == 18 || age_2 != 22) {
-//     console.log(" person is eligible not for vote")
-// }
-
-let country : string [] = ["pakistan", "turkey", "japan", "china"]
-// if (country.includes("pakistan")){
-//    console.log("pakistan is in country list")
-//}
-
-if (!country.includes ("america")) {
-    console.log("america is not in an array")
-}
+let alien_color : string = "red"
+if (alien_color == "red")
+console.log("no output")
