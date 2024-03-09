96f63b0e43086aebcf542ee8942d4005a91d48e7
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:43:35 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-// // Task # 27 : Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
-// • If the alien is green, print a message that the player earned 5 points.
-// • If the alien is yellow, print a message that the player earned 10 points.
-// • If the alien is red, print a message that the player earned 15 points.
-// • Write three versions of this program, making sure each message is printed for the appropriate color alien
-var alien_color = "red";
-if (alien_color == "green") {
-    console.log("5 points");
+// // Task # 28 : Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+// • If the person is less than 2 years old, print a message that the person is a baby.
+// • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+// • If the person is age 65 or older, print a message that the person is an elder.
+var age = 18;
+if (age < 2) {
+    console.log("you are a baby");
 }
-else if (alien_color == "yellow") {
-    console.log("10 points");
+else if (age < 4) {
+    console.log("you are a toddler");
+}
+else if (age < 13) {
+    console.log("you are a kid");
+}
+else if (age < 20) {
+    console.log("you are a teenager");
+}
+else if (age < 65) {
+    console.log("you are a adult");
 }
 else {
-    console.log("15 points");
+    console.log("you are older");
 }
