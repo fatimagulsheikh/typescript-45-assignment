93ce17e3c90d1ece2e2c60696372f9949dbf6def
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:33:28 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 "use strict";
-let Namewithwhitespace = "\n\t FATIMA \t\n";
-console.log( Namewithwhitespace);
-let Namewithoutwhitespace  = Namewithwhitespace.trim();
-console.log( Namewithoutwhitespace);
+/* Task # 7 : Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+Task # 8 : You should create four lines that look like this:
+console.log(5 + 3)
+Your output should simply be four lines with the number 8 appearing once on each line.*/
+
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(2 * 4);
+console.log(16 / 2);
