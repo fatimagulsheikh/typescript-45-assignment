4bc25445e0a50e63eacb2d673f74d9489c0da11e
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:34:30 -0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,9 +1,7 @@
-/* Task # 7 : Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
-Task # 8 : You should create four lines that look like this:
-console.log(5 + 3)
-Your output should simply be four lines with the number 8 appearing once on each line.*/
+/* Task # 9 : Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
 
-console.log(5 + 3);
-console.log(11 - 3);
-console.log(2 + 4);
-console.log(16 / 2);
+Task # 10 (part 1) : Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.*/
+
+const Favorite_Number : number = 7;
+const Message : string = ( `My favorite number is , ${Favorite_Number}` );
+console.log(Message);
