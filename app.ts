1a02f97ad7e012807005569df1956a2b8d0805c2
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:42:37 -0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,13 +1,22 @@
-// // Task # 25 : Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
-// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+// // Task # 26 : Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+// • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
 
-// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+// • Write one version of this program that runs the if block and another that runs the else block.
 
 // let alien_color = "green"
 
-// if (alien_color == "green")
-// console.log("you earn 5 points")
+// if (alien_color == "green") {
+//     console.log("player gust earned 5 points for shooting the alien")
+// } else{
+//     console.log("player just earned 10 points")
+// }
 
-let alien_color : string = "red"
-if (alien_color == "red")
-console.log("no output")
+let alien_color = "red"
+
+if (alien_color == "green") {
+    console.log("player gust earned 5 points for shooting the alien")
+} else{
+    console.log("player just earned 10 points")
+}
