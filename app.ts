486fdceece7c3dd4f6a0e35522cf745c1f3a79e5
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:44:02 -0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,28 +1,28 @@
-// // Task # 28 : Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
-// • If the person is less than 2 years old, print a message that the person is a baby.
+// // Task # 29 : Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
 
-// • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
 
-// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+let favorite_fruits : string [] = ["Orange", "Mango", "Grapes", "kivi", "peach"]
 
-// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
-
-// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
-
-// • If the person is age 65 or older, print a message that the person is an elder.
-
-let age : number = 18
-
-if (age < 2) {
-    console.log("you are a baby")
-} else if (age < 4){
-    console.log("you are a toddler")
-} else if (age < 13){
-    console.log("you are a kid")
-} else if (age < 20){
-    console.log("you are a teenager")
-} else if (age < 65){
-    console.log("you are a adult")
-} else {
-    console.log("you are older")
+if (favorite_fruits.includes("Mango")) {
+    console.log ("Mango")
 }
+
+if (favorite_fruits.includes("Apple")) {
+    console.log ("apple")
+}
+
+if (favorite_fruits.includes("Orange")) {
+    console.log ("Orange")
+}
+
+if (favorite_fruits.includes("peach")) {
+    console.log ("you are really like bananas")
+}
+
+if (favorite_fruits.includes("kivi")) {
+    console.log ("you are really like bananas")
+}
+
+
