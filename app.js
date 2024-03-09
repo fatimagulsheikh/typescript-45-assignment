383977ee783f6e2b18f735cb8eee0d5b05047a81
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:41:15 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-// // Task_22 : Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
-var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-// console.log(days{7});
-console.log(days[6]);
+// // Task # 23 : Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+var car = "subaru";
+console.log("is car == 'subaru'? predict true");
+console.log(car == 'subaru');
+console.log("is car != 'honda city'? predict true");
+console.log(car != 'honda city');
+console.log("is car == 'subaru'? predict false");
+console.log(car == 'subaru');
+console.log("is car == 'SUBARU'? predict flase");
+console.log(car == 'SUBARU');
+console.log("is car.lenght == 3? predict false");
+console.log(car.length == 3);
+console.log("is car.lenght != 10? predict true");
+console.log(car.length != 10);
+console.log("is 10 > 45 ? predict false");
+console.log(10 > 45);
+console.log("is 3 <= 2 ? predict false");
+console.log(3 <= 2);
+console.log("is 72 .= 83 ? predict fals");
+console.log(72 >= 83);
+console.log("is car == 'subaru' && car.lenght == 6? predict true ");
+console.log(car == 'subaru' && car.length == 6);
