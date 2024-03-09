9da13d767c785c52908dcde74b1f4e1c73bf3132
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-09 15:45:05 -0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,14 +1,23 @@
-// // Task # 30 : Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
-// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// // Task # 31 : No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
 
-// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
 
 let users : string [] = ["admin", "eric", "haseeb", "nadeem", "fatima"]
 
-for(let user of users) {
-    if (user === "admin") {
-        console.log("Hello admin, would you like to see a status report?")
-    } else {
-        console.log(` Hello ${user}, thank you for logging in again`)
-    }
+// if (users.length === 0) {
+//    console.log ("We need to find some users!")
+// } else {
+//     for (let user of users) { 
+//         if ( user === "admin") {
+//                 console.log("Hello admin, would you like to see a dtatus report?")
+//         } else {
+//             console.log(`Hello ${user}, thank you for logging in again`)
+//         }
+//     }
+// }
+
+users = []
+if (users.length === 0) {
+    console.log ("we need to find some users!")
 }
